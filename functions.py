b2d4c1b3cf77f761baa693b3996ca8590561ed26
EscribanoVegaJose <escribanovegajose@gmail.com>
2021-01-02 02:27:05 +0100

    functions.py added

--- functions.py
+++ functions.py
@@ -2,3 +2,31 @@
 # ¿Qué es una función y para qué sirve?, normas de un código limpio
 # Estructura de una función, parámetros de entrada y salida 
 # Ejemplo de las diferentes combinaciones
+itemsAvailableList= ["sartén", "hacha", "ruedas", "té"]
+
+
+# no param de entrada, no devuelve nada
+def welcomeMessage():
+  print("Bienvenido a shopping market, cargando información...")
+  print("por favor, introduzca su nombre de usuario: ")
+  
+# sí param de entrada, no devuelve nada
+def requestUsername(username):
+  print ("nombre de usuario disponible, "+username)
+  
+
+
+# sí param de entrada, sí devuelve un valor
+def isAvailable (item):
+  return item in itemsAvailableList
+  
+welcomeMessage()
+userName = input()
+requestUsername(userName)
+print("por favor, introduzca el producto que desea comprobar: ")
+userBuy= input()
+print (isAvailable(userBuy))
+
+# estructura -> def nombre_función (parámetro 1, parámetro 2)
+
+ 
